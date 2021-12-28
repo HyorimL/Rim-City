1ef7f289bea0851222f965e4f2ed222db6782442
admin <admin@YD5-02>
2021-12-28 09:05:17 +0900

    first

---
+++ hellojava/src/com/yedam/java/main/Hellojava.java
@@ -0,0 +1,15 @@
+package com.yedam.java.main;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to JAVA");
+        System.out.println("Lee Hyorim");
+        System.out.println("hi");
+        
+      
+        
+	}
+
+}
